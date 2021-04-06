e70b5a3812d46ce0e7a37ee24e7308de7900587d
Mark Lin <61925580+Marklin2289@users.noreply.github.com>
2021-04-05 23:14:59 -0700

    clean up

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,4 +1,4 @@
-import { useRouter } from "next/router";
+// import { useRouter } from "next/router";
 import Link from "next/link";
 import { server } from "../../../config";
 import Meta from "../../../components/Meta";
