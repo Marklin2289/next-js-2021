7ac44172f0e31dd76ccfc045d4429e95f8eb30a4
Mark Lin <61925580+Marklin2289@users.noreply.github.com>
2021-04-05 17:33:58 -0700

    setup and clean up

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Index Page</title>
+        <meta name="keywords" content="web development, programming" />
+      </Head>
+      <h1>Welcome to nextjs</h1>
+    </div>
+  );
+}
